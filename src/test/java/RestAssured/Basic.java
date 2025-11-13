3f3dbde071490a1503914cf23a0df96d17921cdc
KASAUDHAN <lav.kasaudhan@agilent.com>
2025-11-13 16:18:35 +0530

    2nd commit

--- src/test/java/RestAssured/Basic.java
+++ src/test/java/RestAssured/Basic.java
@@ -1,6 +1,7 @@
 package RestAssured;
 
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 
 public class Basic {
 
@@ -8,11 +9,33 @@ public class Basic {
 		// validate if Add Place API is working as expected
 		
 		//given - all input details
-		//when - Submit the API
+		//when - Submit the API  -resource, http method
 		//then - Validate the response
 		
 		RestAssured.baseURI="https://rahulshettyacademy.com";
-
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body(" { \r\n"
+				+ " \"location\": { \r\n"
+				+ " \"lat\": -38.383494, \r\n"
+				+ " \"lng\": 33.427362 \r\n"
+				+ " }, \r\n"
+				+ " \"accuracy\": 50, \r\n"
+				+ " \"name\": \"Rahul Shetty Academy\", \r\n"
+				+ " \"phone_number\": \"(+91) 983 893 3937\", \r\n"
+				+ " \"address\": \"29, side layout, cohen 09\", \r\n"
+				+ " \"types\": [ \r\n"
+				+ " \"shoe park\", \r\n"
+				+ " \"shop\" \r\n"
+				+ " ], \r\n"
+				+ " \"website\": \"https://rahulshettyacademy.com\", \r\n"
+				+ " \"language\": \"French-IN\" \r\n"
+				+ " } \r\n"
+				+ "")
+		.when().post("maps/api/place/add/json")
+		.then().log().all().assertThat().statusCode(200);
+		
+		
+		
 	}
 
 }
